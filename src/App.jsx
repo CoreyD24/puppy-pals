79667b7d8c80e84309c8449acbfcaa9258c8083d
coreyfsa <coreyfsa@DESKTOP-TCPPHAT.localdomain>
2023-11-13 13:41:18 -0700

    Second commit. Puppylist renders to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList)
 
   return (
-    <>
-    </>
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
   )
 }
 
